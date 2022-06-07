4e17b8c4a72b355d6c6f785214ea6151dd584cba
Tristan <tsilvas0113@gmail.com>
2022-06-07 16:01:04 -0500

    Create js function to change sketchpad size
    Function will prompt user to enter a size less than 100 and use that
    input to create a new sketchpad size. Will ask again if user enters a
    number greater than 100 or just 'return' if user continues to input
    wrong size.
    
    Also linked button to the function using js.

--- javascript.js
+++ javascript.js
@@ -13,5 +13,24 @@ function changeColor(e) {
     e.target.style.background = 'yellow';
 }
 
+function changePad() {
+    let userChoice = prompt('How big do you want the sketch pad? (Limit 100)');
+    if (userChoice > 100) {
+        userChoice = prompt('That size is too big! Must be smaller than or equal to 100!');
+        if (userChoice > 100) {
+            return;
+        }
+    }
+
+    const padContainer = document.querySelector('.pad-container');
+    while (padContainer.hasChildNodes()) {
+        padContainer.removeChild(padContainer.firstChild);
+    }
+    grid(userChoice);
+}
+
 let gridSize = 16;
 grid(gridSize);
+
+const btn = document.querySelector('.grid-change');
+btn.addEventListener('click', changePad);
